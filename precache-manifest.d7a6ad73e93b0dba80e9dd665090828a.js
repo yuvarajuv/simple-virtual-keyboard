9be7b4d97248377171691c9a19ecba4d6ad3a9e9
Admin <admin@BV-LM03-MacBook-Air.local>
2020-10-16 18:36:20 +0530

    Updates

---
+++ precache-manifest.d7a6ad73e93b0dba80e9dd665090828a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939855f1cc0ba4a41f2e335db4d69af2",
+    "url": "/simple-virtual-keyboard/index.html"
+  },
+  {
+    "revision": "794e0cf5678533eb3f08",
+    "url": "/simple-virtual-keyboard/static/css/main.8b586cac.chunk.css"
+  },
+  {
+    "revision": "5de5178dfdd9ad9e51ac",
+    "url": "/simple-virtual-keyboard/static/js/2.d48f9a20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-virtual-keyboard/static/js/2.d48f9a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794e0cf5678533eb3f08",
+    "url": "/simple-virtual-keyboard/static/js/main.0626fb11.chunk.js"
+  },
+  {
+    "revision": "b25890aeae466aa7398f",
+    "url": "/simple-virtual-keyboard/static/js/runtime-main.f7a005d3.js"
+  }
+]);
