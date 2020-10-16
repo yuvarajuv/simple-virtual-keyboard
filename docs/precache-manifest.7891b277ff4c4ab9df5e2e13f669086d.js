719bd127ce461210c7177782673586c110b5c9d6
Admin <admin@BV-LM03-MacBook-Air.local>
2020-10-16 17:57:50 +0530

    Initial commit

---
+++ docs/precache-manifest.7891b277ff4c4ab9df5e2e13f669086d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff7a574e21a8b069bf32b42b414eec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b630ef13910361de4be",
+    "url": "/static/css/main.8b586cac.chunk.css"
+  },
+  {
+    "revision": "5de5178dfdd9ad9e51ac",
+    "url": "/static/js/2.d48f9a20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d48f9a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b630ef13910361de4be",
+    "url": "/static/js/main.d3d8898f.chunk.js"
+  },
+  {
+    "revision": "42076e8530639090cab3",
+    "url": "/static/js/runtime-main.32434b08.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      position: 0
+    };
+    this.keys = [
+      [{ name: 'Backspace', value: 'backspace', className: 'specialKey' }],
+      [{ name: '7', value: 7, className: 'keyItem' }, { name: '8', value: 8, className: 'keyItem' }, { name: '9', value: 9, className: 'keyItem' }],
+      [{ name: '4', value: 4, className: 'keyItem' }, { name: '5', value: 5, className: 'keyItem' }, { name: '6', value: 6, className: 'keyItem' }],
+      [{ name: '1', value: 1, className: 'keyItem' }, { name: '2', value: 2, className: 'keyItem' }, { name: '3', value: 3, className: 'keyItem' }],
+      [{ name: '0', value: 0, className: 'keyItem' }, { name: '.', value: '.', className: 'keyItem' }, { name: '-', value: '-', className: 'keyItem' }],
+      [{ name: '←', value: 'left', className: 'specialKey' }, { name: '→', value: 'right', className: 'specialKey' }],
+      [{ name: 'Clear All', value: 'clear', className: 'specialKey' }],
+    ];
+  }
+
+  onChange = (event) => {
+    const { value } = event.target;
+    this.setState({
+      value
+    });
+  }
+
+  onKeyUp = (event) => {
+    const position = event.target.selectionStart;
+    this.setState({
+      position
+    });
+  }
+
+  setCaretPosition = (elem, caretPos) => {
+    if (elem.createTextRange) {
+      const range = elem.createTextRange();
+      range.move('character', caretPos);
+      range.select();
+    } else if (elem.selectionStart || elem.selectionStart === 0) {
+      elem.focus();
+      elem.setSelectionRange(caretPos, caretPos);
+    } else elem.focus();
+  }
+
+  onKeyEvent = (event, key) => {
+    event.preventDefault();
+    let elem = document.getElementById('keyBoard');
+    let chars = this.state.value.split('');
+    let position = 0;
+
+    if (key === 'left') {
+      // left arrow event
+      position = (this.state.position - 1) > 0 ? this.state.position - 1 : 0;
+    } else if (key === 'right') {
+      // right arrow event
+      const nextPostion = this.state.position + 1;
+      position = nextPostion > this.state.value.length ? this.state.value.length : nextPostion;
+    } else if (key === 'backspace') {
+      // backspace
+      const currentPosition = elem.selectionStart;
+      if (currentPosition > 0) {
+        chars.splice(currentPosition - 1, 1);
+        position = currentPosition - 1;
+      }
+    } else if (key === 'clear') {
+      // clear all
+      chars = [];
+      position = 0;
+    } else {
+      const cusorPosition = elem.selectionStart || 0;
+      chars.splice(cusorPosition, 0, key);
+      position = cusorPosition + 1;
+      elem = document.getElementById('keyBoard');
+    }
+
+    const value = chars.join('');
+    this.setState({
+      value,
+      position
+    }, () => {
+      this.setCaretPosition(elem, position);
+    });
+  };
+
+
+  render() {
+    return (
+      <div className="keyboard-container">
+        <h2>Simple Virtual keyboard With Arrows</h2>
+        <div className="inputContainer">
+          <input id="keyBoard" onChange={this.onChange} value={this.state.value} onKeyUp={this.onKeyUp} />
+        </div>
+        <div className="keyboard">
+          {
+            this.keys.map((items, index) => (
+              <div className="keys" key={`keyGroup-${index}`}>
+                {
+                  items.map(item => (
+                    <button key={item.value} type="button" className={item.className} onClick={(e) => { this.onKeyEvent(e, item.value); }}>
+                      {item.name}
+                    </button>
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+
+};
 
 export default App;
